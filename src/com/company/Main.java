2209005f1ceed2cfadab9c9bdede654eec3a68bc
Bulygin <jbulygin@yandex.ru>
2020-02-08 13:59:13 +0300

    Final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import com.company.service.DecryptionService;
+
 public class Main {
 
-    public static void main(String[] args) {
-	      // write your code here
+  private static final String CIPHER_TEXT =
+      "Еъёчхф Вхзёюлх, адздёиу ф ждэщхб, црбх еёдюэчъщъгюъв южаижжзчх, "
+          + "ждчъёнъгжзчдв. Ъы зёюивй жёхчгюв бюнс ж ъы вдгивъгзхбсгрв аёхкдв. Зъеъёс вгъ дмъчющгд, мзд гъюэцъьгджзс тздшд "
+          + "аёхкх фчбфъзжф жбъщжзчюъв гъждчъёнъгжзчх мъбдчъмъжадшд югщючющиивх. Ф юэимюб чхни южздёюу ю чгыж юэвъгъгюф, "
+          + "здмгъъ дзёхэюч еджздфггиу юэвъгмючджзс мъбдчъмъжаюк едёдадч. Ю зъв гъ въгъъ, еджбъщдчхбх гъищхмх. Ф еёюнъб а "
+          + "чрчдщи, мзд чюгдя чжъви вдя югзъббъаз, х чдэвдьгд, вды мёъэвъёгдъ жзёъвбъгюъ ад чжъви шхёвдгюмгдви. Гхязю "
+          + "ёънъгюъ вгъ едвдшбх еёдшёхввх югзиюзючгдшд зюех, жеълюхбсгд ждэщхггхф щбф юэимъгюф деёъщъбъггрк жздёдг"
+          + " мъбдчъмъжадя щиню. Въгф вдьгд гхэчхзс дзлдв Вхзёюлр, х ъы, цъэ еёъичъбюмъгюф, вхзъёсу.";
+
+  public static void main(String[] args) {
+    DecryptionService decryptionService = new DecryptionService(CIPHER_TEXT);
+    for (int i = 0; i <= 32; i++) {
+      System.out.printf("%d:\t", i);
+      decryptionService.printDecrypted(i);
     }
+  }
 }
